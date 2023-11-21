38858a1e22914d8d36a854390e4fec4fa5fa6648
ido <ido.pluto@w-on.uk>
2023-11-21 20:26:20 +0200

    fix: site base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,12 @@ import expressiveCode from "astro-expressive-code";
 // https://astro.build/config
 export default defineConfig({
   integrations: [expressiveCode(), starlight({
+    site: 'https://withastro-utils.github.io',
+    base: '/docs',
+    editLink: {
+      baseUrl: 'https://github.com/withastro-utils/docs/tree/main/',
+    },
+    lastUpdated: true,
     favicon: '/favicon.png',
     title: 'Astro Utils',
     logo: {
